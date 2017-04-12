3a03c62dd866bb8f4da4a9772c90d119ff1e2ad2
TeeJaySky <trentjsky@gmail.com>
2017-04-12 23:38:27 +1000

    Adding some initial controls and file open/csv interpretting

--- Reviewer/Reviewer/Form1.Designer.cs
+++ Reviewer/Reviewer/Form1.Designer.cs
@@ -28,20 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchTerm = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.category = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bsr = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.url = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1210, 783);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1358, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 54);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1228, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Result file directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1263, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Search Term";
+            // 
+            // searchTerm
+            // 
+            this.searchTerm.Location = new System.Drawing.Point(1358, 98);
+            this.searchTerm.Name = "searchTerm";
+            this.searchTerm.Size = new System.Drawing.Size(317, 22);
+            this.searchTerm.TabIndex = 4;
+            // 
+            // title
+            // 
+            this.title.Location = new System.Drawing.Point(1358, 126);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(317, 22);
+            this.title.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1315, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Title";
+            // 
+            // category
+            // 
+            this.category.Location = new System.Drawing.Point(1358, 154);
+            this.category.Name = "category";
+            this.category.Size = new System.Drawing.Size(317, 22);
+            this.category.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1286, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Category";
+            // 
+            // bsr
+            // 
+            this.bsr.Location = new System.Drawing.Point(1358, 182);
+            this.bsr.Name = "bsr";
+            this.bsr.Size = new System.Drawing.Size(317, 22);
+            this.bsr.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1313, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "BSR";
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(1358, 210);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(317, 22);
+            this.url.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1314, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "URL";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1059, 682);
+            this.ClientSize = new System.Drawing.Size(1687, 807);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.bsr);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.category);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.searchTerm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox searchTerm;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox category;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox bsr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Reviewer/Reviewer/Form1.cs
+++ Reviewer/Reviewer/Form1.cs
@@ -15,6 +15,79 @@ namespace Reviewer
         public Form1()
         {
             InitializeComponent();
+
+            this.KeyPress += new KeyPressEventHandler(Form1_KeyPress);
+
+            textBox1.Text = @"C:\Users\Trent\Desktop\TEmp\Outspoken Panda\Results.csv";
+
+            using(System.IO.StreamReader reader = new System.IO.StreamReader(textBox1.Text))
+            {
+                int lineNumber = 0;
+                string line;
+                while((line = reader.ReadLine())!= null)
+                {
+                    lineNumber++;
+
+                    if (lineNumber == 1)
+                    {
+                        continue;
+                    }
+
+                    if (lineNumber == 2)
+                    {
+                        var results = line.Split(',').ToList();
+
+                        searchTerm.Text = results[0];
+                        title.Text = results[1];
+                        category.Text = results[2];
+                        bsr.Text = results[3];
+                        url.Text = results[4];
+
+                        DisplayWebPage(url.Text); ;
+
+                        break;
+                    }
+                }
+            }
         }
+
+        public void DisplayWebPage(string url)
+        {
+            webBrowser.Navigate(url);
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        // Detect all numeric characters at the form level and consume 1, 
+        // 4, and 7. Note that Form.KeyPreview must be set to true for this
+        // event handler to be called.
+        void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 10)
+            {
+                Console.WriteLine("test");
+            }
+            //if (e.KeyChar >= 48 && e.KeyChar <= 57)
+            //{
+            //    MessageBox.Show("Form.KeyPress: '" +
+            //        e.KeyChar.ToString() + "' pressed.");
+
+            //    switch (e.KeyChar)
+            //    {
+            //        case (char)49:
+            //        case (char)52:
+            //        case (char)55:
+            //            MessageBox.Show("Form.KeyPress: '" +
+            //                e.KeyChar.ToString() + "' consumed.");
+            //            e.Handled = true;
+            //            break;
+            //    }
+            //}
+        }
+
+
     }
 }
